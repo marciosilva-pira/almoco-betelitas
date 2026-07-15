04c8c5025901e46782eba2241b0d8dfb92667ca7
Marcio Silva <marciosilva.pira@gmail.com>
2026-07-15 17:06:23 -0300

    versao ok - 11

--- app/casas/layout.tsx
+++ app/casas/layout.tsx
@@ -1,5 +1,10 @@
+// app/casas/layout.tsx
 import DashboardTemplate from "../dashboard/DashboardTemplate";
 
 export default function LayoutCasas({ children }: { children: React.ReactNode }) {
-  return <DashboardTemplate>{children}</DashboardTemplate>;
+  return (
+    <DashboardTemplate>
+      {children}
+    </DashboardTemplate>
+  );
 }
