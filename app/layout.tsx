e2e121cb5c0e9b0ecd7aa9cd80f7eb2344ae7232
Marcio Silva <marciosilva.pira@gmail.com>
2026-07-15 17:15:04 -0300

    versao ok - 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <body className="bg-gray-100 min-h-screen">
         {children}
       </body>
     </html>
