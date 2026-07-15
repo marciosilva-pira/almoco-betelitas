7c52e2a38f0ef10770b557752b4dfe64cf18b4be
Marcio Silva <marciosilva.pira@gmail.com>
2026-07-15 16:01:42 -0300

    versao ok - 2

--- app/casas/page.tsx
+++ app/casas/page.tsx
@@ -1,12 +1,6 @@
 // app/casas/page.tsx
-// Altere a linha 2 para:
-import DashboardTemplate from "../dashboard/DashboardTemplate";
 import CasasContent from "../components/CasasContent";
 
 export default function Page() {
-  return (
-    <DashboardTemplate>
-      <CasasContent />
-    </DashboardTemplate>
-  );
+  return <CasasContent />;
 }
